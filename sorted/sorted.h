b4e2f324b15327a159c223fcfc4daf3ad0857c17
Str1kez <vazinov@yandex.ru>
2021-07-26 18:15:36 +0300

    need to create algo for 3 elems

--- sorted/sorted.h
+++ sorted/sorted.h
@@ -7,5 +7,6 @@ void	sort_array(int *arr, int begin, int end);
 int		*array_for_order(t_list *stack);
 void	set_order_index(t_list *stack);
 int		is_sorted(t_list *stack);
+void	low_stack(t_list **stack_a, t_list **stack_b, t_cmd **cmd);
 
 #endif //FT_PRINTF_SORTED_H
