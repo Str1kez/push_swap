214d5ad01d73b3e0e2424214d41d28dc31b9368a
Str1kez <vazinov@yandex.ru>
2021-07-30 23:49:04 +0300

    need to check norm, added algo for < 6 elems

--- sorted/low_stack.c
+++ sorted/low_stack.c
@@ -1,14 +1,62 @@
 #include "sorted.h"
 
+static	void	min_to_stack_b(t_list **stack_a, t_list **stack_b, t_cmd **cmd, int min_order)
+{
+	if ((*stack_a)->order != min_order)
+	{
+		if ((*stack_a)->next->next->next->order == min_order)
+		{
+			if (ft_lstsize(*stack_a) == 5)
+			{
+				reverse_rotate(stack_a);
+				ft_cmdadd_back(cmd, ft_newcmd(8));
+			}
+			reverse_rotate(stack_a);
+			ft_cmdadd_back(cmd, ft_newcmd(8));
+		}
+		else
+		{
+			if ((*stack_a)->next->next->order == min_order)
+			{
+				rotate(stack_a);
+				ft_cmdadd_back(cmd, ft_newcmd(5));
+			}
+			rotate(stack_a);
+			ft_cmdadd_back(cmd, ft_newcmd(5));
+		}
+	}
+	push_top(stack_b, stack_a);
+	ft_cmdadd_back(cmd, ft_newcmd(4));
+}
+
 static	void	two_in_stack(t_list **stack, t_cmd **cmd)
 {
 	ft_lstswap(*stack);
 	ft_cmdadd_back(cmd, ft_newcmd(0));
 }
 
-static	void	three_in_stack(t_list **stack, t_cmd **cmd)
+static	void	four_in_stack(t_list **stack_a, t_list **stack_b, t_cmd **cmd)
 {
-	// TODO: Realize this
+	min_to_stack_b(stack_a, stack_b, cmd, 1);
+	three_in_stack(stack_a, cmd, 4);
+	push_top(stack_a, stack_b);
+	ft_cmdadd_back(cmd, ft_newcmd(3));
+}
+
+static	void	five_in_stack(t_list **stack_a, t_list **stack_b, t_cmd **cmd)
+{
+	if ((*stack_a)->next->next->next->next->order == 1)
+	{
+		reverse_rotate(stack_a);
+		ft_cmdadd_back(cmd, ft_newcmd(8));
+	}
+	min_to_stack_b(stack_a, stack_b, cmd, 1);
+	min_to_stack_b(stack_a, stack_b, cmd, 2);
+	three_in_stack(stack_a, cmd, 5);
+	push_top(stack_a, stack_b);
+	ft_cmdadd_back(cmd, ft_newcmd(3));
+	push_top(stack_a, stack_b);
+	ft_cmdadd_back(cmd, ft_newcmd(3));
 }
 
 void	low_stack(t_list **stack_a, t_list **stack_b, t_cmd **cmd)
@@ -19,5 +67,9 @@ void	low_stack(t_list **stack_a, t_list **stack_b, t_cmd **cmd)
 	if (size_a == 2)
 		two_in_stack(stack_a, cmd);
 	if (size_a == 3)
-		three_in_stack(stack_a, cmd);
+		three_in_stack(stack_a, cmd, 3);
+	if (size_a == 4)
+		four_in_stack(stack_a, stack_b, cmd);
+	if (size_a == 5)
+		five_in_stack(stack_a, stack_b, cmd);
 }
--- src/src.h
+++ src/src.h
@@ -9,7 +9,6 @@
 
 void	ft_putstr(char *s);
 void	print_command(int cmd);
-void	swap_top(t_list *stack);
 void	push_top(t_list **put, t_list **take);
 void	rotate(t_list **stack);
 void	reverse_rotate(t_list **stack);
